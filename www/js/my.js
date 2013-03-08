abde9245979d167eb2a1dd97af6cfa1d186acd34
Rafael Bravo <rafael@metheora.com>
2013-03-07 21:28:58 -0300

    getting nice

--- www/js/my.js
+++ www/js/my.js
@@ -61,4 +61,77 @@ $(function () {
             $(p).addClass('active');
         }
     });
+
+
+
+	var elevator;
+	var myOptions = {
+	    zoom: 11,
+	    center: new google.maps.LatLng(-22.907072809355967, -43.21398052978515),
+	    mapTypeId: 'satellite'
+	};
+
+	map = new google.maps.Map($('#map_canvas')[0], myOptions);
+	var markers = [];
+
+	var directionDisplay;
+	var directionsService = new google.maps.DirectionsService();
+	var map;
+	var rendererOptions = {
+	  draggable: true
+	};
+	directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+	directionsDisplay.setMap(map);
+
+	function calcRoute() {
+	    var start = document.getElementById('start').value;
+	    var end = document.getElementById('end').value;
+	    var request = {
+	        origin:start,
+	        destination:end,
+	        travelMode: google.maps.DirectionsTravelMode.DRIVING
+	        //provideRouteAlternatives: false,
+	        //unitSystem: UnitSystem.IMPERIAL
+	    };
+	    
+	    var waypoints = [];
+	        
+	     if($('#carona1').val() != "")
+	     {
+	         waypoints.push({
+	          location:$('#carona1').val(),
+	          stopover:true
+	        })
+	     }
+	    
+	    
+	     if($('#carona2').val() != "")
+	     {
+	         waypoints.push({
+	          location:$('#carona2').val(),
+	          stopover:true
+	        })
+	     }
+	    
+	    if(waypoints.length > 0)
+	        request.waypoints = waypoints;
+	    
+	    directionsService.route(request, function(response, status) {
+	        if (status == google.maps.DirectionsStatus.OK) {
+	            directionsDisplay.setDirections(response);
+	        }
+	    });
+	}
+
+	calcRoute();
+
+	$('input[type=text]').change(calcRoute);
+
+	$('a[href="#mapa"]').click(function(){
+		setTimeout(function(){
+			google.maps.event.trigger(map, 'resize');
+		},500);
+	});
 });
+
+
